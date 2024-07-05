6900025bdaef3b60fbaa8d47d951935c4557dd7c
root <root@TasnimSomo>
2024-07-05 07:57:08 +0400

    new commit

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 """ Basic annotations - floor """
-import math
 
 
 def floor(n: float) -> float:
-    return math.floor(n)
+    """Returns the floor of the float"""
+    return int(n)
