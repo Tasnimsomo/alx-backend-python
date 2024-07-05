f51c4ca54f4e67ff5ab9d47e0075029c1374d69f
root <root@TasnimSomo>
2024-07-05 07:48:15 +0400

    new commit

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+""" Basic annotations - floor """
+import math
+def floor(n: float) -> float:
+    return math.floor(n)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+""" Basic annotations - to string """
+def to_str(n: float) -> str:
+    return str(n)
