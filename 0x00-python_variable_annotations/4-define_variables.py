b436c7cf8963eb12655c79c16918c24756cc8508
root <root@TasnimSomo>
2024-07-05 08:02:39 +0400

    new commit

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -6,7 +6,8 @@ i_understand_annotations, a boolean with a value of True
 school, a string with a value of “Holberton”
 """
 
+
 a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
-school: str = 'Holberlton'
+school: str = 'Holberton'
