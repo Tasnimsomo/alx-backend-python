f260b32d62a85388c25352a2f9b98d224709a7fa
root <root@TasnimSomo>
2024-07-08 21:40:40 +0400

    0-basic_async_syntax.py

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""  Let's execute multiple coroutines """
+
+import async
+import wait_random from ./0-basic_async_syntax
+from typing import List
+
+async def wait_n(n: int, max_delay: int) -> LIST[float]:
+    """ spawn wait_random n times """
+    delays = []
+
+    for i in range(n):
+        wait_random(max_delays) += delays
+        results = await asyncio.gather(*delays)
+        return results
